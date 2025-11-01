05cca34c06979bbacba324b7196c85168278d0bd
RomNeedBoba <rpphyrom.dev@gmail.com>
2025-11-01 12:08:24 +0700

    chob error plz i fix all script

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -6,17 +6,18 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-gray-900/70 backdrop-blur-md shadow-sm">
-      <div className="mx-auto flex max-w-[1440px] items-center justify-between px-6 py-5">        
+      <div className="mx-auto flex max-w-[1440px] items-center justify-between px-6 py-5">
 
         {/* Left: Logo + Menu together */}
         <div className="flex items-center gap-10">
           <Image
-            src="/images/raw.png"
+            src="/images/Raw.png"
             alt="Logo"
             width={50}
             height={40}
             className="object-contain"
           />
+
 
           <nav className="flex items-center gap-8 text-gray-200 font-medium">
             <Link href="/resources" className="hover:text-white">Resources</Link>
